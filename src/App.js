a6cf1459d7433086c5ec592d7a3ba80394ecbbb4
dstollbyu <elderstoll@gmail.com>
2020-06-26 16:25:17 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -16,9 +17,36 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React For
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import React from 'react';
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/"
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org"
+  }
+]
+
+function App() {
+
+  return (
+    <div>
+      {list.map((item) => {
+        return <div>{item.title}</div>;
+      })}
     </div>
   );
 }
