a1b4568f8a64e0120e37d14dd3b52c4b3d7c68a5
dstollbyu <elderstoll@gmail.com>
2020-07-01 15:48:09 -0600

    React fragments
    Added fragments that are usable in JSX

--- src/App.js
+++ src/App.js
@@ -89,10 +89,10 @@ const App = () => {
 };
 
 const Search = ({onSearch, search}) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" value={search} onChange={onSearch} />
-  </div>
+  </>
 );
 
 
