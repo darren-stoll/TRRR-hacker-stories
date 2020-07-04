b28b68732793b88ee5042d2976599154f1e27a62
dstollbyu <elderstoll@gmail.com>
2020-07-03 19:22:12 -0600

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -48,6 +48,15 @@ const initialStories = [
   }
 ];
 
+const getAsyncStories = () => 
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -64,7 +73,13 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
