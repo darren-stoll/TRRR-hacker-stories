7473b314253a58fba29d009e8ebc0f79a4ca0c3c
dstollbyu <elderstoll@gmail.com>
2020-07-15 10:15:14 -0600

    CSS in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import './App.css';
+
 // Custom hook to manage state yet synchronize with local storage, hence semi persistent.
 const useSemiPersistentState = (key, initialState) => {
   // Define the value variable and setValue function from a useState hook. If a value already exists, set it to that. Otherwise, go to initialState
@@ -118,22 +120,22 @@ const App = () => {
 
   // Component definition for SearchForm to be used in return statement
   const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-    <form onSubmit={onSearchSubmit}>
+    <form onSubmit={onSearchSubmit} className="search-form">
       <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={onSearchInput}>
         <strong>Search:</strong>
       </InputWithLabel>
 
-      <button type="submit" disabled={!searchTerm}>Submit</button>
+      <button type="submit" disabled={!searchTerm} className="button button_large">Submit</button>
     </form>
   );
 
   // Return code that will be used in index.js
   return (  
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit} />
-      <hr />
+      
       {/* If an error is present in the stories state, return error message */}
       {stories.isError && <p>Something went wrong ... </p>}
       {/* Else if stories state is in loading, return loading message */}
@@ -163,9 +165,9 @@ const InputWithLabel = ({ id, type="text", value, onInputChange, isFocused, chil
 
   return(
     <>
-      <label htmlFor={id}>{children}</label>&nbsp;
+      <label htmlFor={id} className="label">{children}</label>&nbsp;
       {/* Pass the ref object into the ref attribute of the input tag */}
-      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
+      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} className="input" />
     </>
   )  
 }
@@ -177,15 +179,15 @@ const List = ({ list, onRemoveItem }) =>
 // Item component that shows a story's details
 const Item = ( { item, onRemoveItem } ) => {
   return (
-    <div>
-      <span>
+    <div className="item">
+      <span style={{ width: '40%' }}>
         <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a> {/* noopener and noreferrer prevents hacking when target is blank */}
       </span>
-      <span> {item.author} </span>
-      <span>{item.num_comments} </span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments} </span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button type="button" onClick={() => onRemoveItem(item)} className="button button_small">
           Dismiss
         </button>
       </span>
