f5833b4deb2d876b8666e5b70d3dfa55cceba2cd
dstollbyu <elderstoll@gmail.com>
2020-07-20 12:41:10 -0600

    Testing in React: Integration Testing - Component

--- src/App.test.js
+++ src/App.test.js
@@ -35,6 +35,143 @@ const stories = [storyOne, storyTwo];
 
 jest.mock('axios');
 
+
+// Integration Testing: Component
+describe('App', () => {
+    test('succeeds fetching data', async () => {
+        const promise = Promise.resolve({
+            data: {
+                hits: stories,
+            },
+        });
+
+        axios.get.mockImplementationOnce(() => promise);
+
+        render(<App />);
+
+        expect(screen.queryByText(/Loading/)).toBeInTheDocument();
+
+        await act(() => promise);
+
+        expect(screen.queryByText(/Loading/)).toBeNull();
+
+        expect(screen.getByText('React')).toBeInTheDocument();
+        expect(screen.getByText('Redux')).toBeInTheDocument();
+        expect(screen.getAllByText('check.svg').length).toBe(2);
+    });
+
+    test('fails fetching data', async () => {
+      const promise = Promise.reject();
+
+      axios.get.mockImplementationOnce(() => promise);
+
+      render(<App />);
+
+      expect(screen.getByText(/Loading/)).toBeInTheDocument();
+
+      try {
+        await act(() => promise);
+      } catch (error) {
+        expect(screen.queryByText(/Loading/)).toBeNull();
+        expect(screen.queryByText(/went wrong/)).toBeInTheDocument();
+      }
+    });
+
+    test(('removes a story'), async () => {
+      const promise = Promise.resolve({
+        data: {
+          hits: stories,
+        }
+      });
+
+      axios.get.mockImplementationOnce(() => promise);
+
+      render(<App />);
+
+      await act(() => promise);
+
+      expect(screen.getAllByText('check.svg').length).toBe(2);
+      expect(screen.getByText('Jordan Walke')).toBeInTheDocument();
+      fireEvent.click(screen.getAllByText('check.svg')[0]);
+
+      expect(screen.getAllByText('check.svg').length).toBe(1);
+      expect(screen.queryByText('Jordan Walke')).toBeNull();
+      
+    });
+
+    test('searches for specific stories', async () => {
+      const reactPromise = Promise.resolve({
+        data: {
+          hits: stories,
+        }
+      });
+
+      const anotherStory = {
+        title: 'JavaScript',
+        url: 'https://en.wikipedia.org/wiki/JavaScript',
+        author: 'Brendan Eich',
+        num_comments: 15,
+        points: 10,
+        objectID: 3
+      };
+
+      const javascriptPromise = Promise.resolve({
+        data: {
+          hits: [anotherStory]
+        }
+      });
+
+      axios.get.mockImplementation(url => {
+        if (url.includes('React')) {
+          return reactPromise;
+        }
+
+        if (url.includes('JavaScript')) {
+          return javascriptPromise;
+        }
+
+        throw Error();
+      });
+
+      // Initial Render
+      render(<App />);
+
+      // First Data Fetching
+      await act(() => reactPromise);
+
+      expect(screen.queryByDisplayValue('React')).toBeInTheDocument();
+      expect(screen.queryByDisplayValue('JavaScript')).toBeNull();
+
+      expect(screen.queryByText('Jordan Walke')).toBeInTheDocument();
+
+      expect(screen.queryByText('Dan Abramov, Andrew Clark')).toBeInTheDocument();
+      expect(screen.queryByText('Brendan Eich')).toBeNull();
+
+      // User Interaction -> Search
+      fireEvent.change(screen.queryByDisplayValue('React'), {
+        target: { 
+          value: 'JavaScript'
+        }
+      });
+
+      expect(screen.queryByDisplayValue('React')).toBeNull();
+      expect(screen.queryByDisplayValue('JavaScript')).toBeInTheDocument();
+
+      fireEvent.submit(screen.queryByText('Submit'));
+
+      // Second Data Fetching
+
+      await act(() => javascriptPromise);
+
+      expect(screen.queryByText('Jordan Walke')).toBeNull();
+      expect(screen.queryByText('Dan Abramov, Andrew Clark')).toBeNull();
+      expect(screen.queryByText('Brendan Eich')).toBeInTheDocument();
+
+    });
+});
+
+// Unit testing: component
+
 // describe('storiesReducer', () => {
 //   test('removes a story from all stories', () => {
 //     const action = { type: 'REMOVE_STORY', payload: storyOne };
