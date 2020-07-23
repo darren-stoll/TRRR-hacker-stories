ee0792364c67abf95c8dd7d6db2d919bb42ebfae
dstollbyu <elderstoll@gmail.com>
2020-07-23 14:28:40 -0600

    Allowing build to be passed so that netlify can grab from it

---
+++ build/precache-manifest.b9405f479d239fe1b2ff3e5bf9e09256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ea8c6df8f6d2a409105ac32008c46e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e347504b56f21933aef",
+    "url": "/static/css/main.65fa6ae6.chunk.css"
+  },
+  {
+    "revision": "ed313217e472789e8f73",
+    "url": "/static/js/2.115aad5f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.115aad5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e347504b56f21933aef",
+    "url": "/static/js/main.7cdefa83.chunk.js"
+  },
+  {
+    "revision": "b55014f044bba798a831",
+    "url": "/static/js/runtime-main.50faa5bf.js"
+  },
+  {
+    "revision": "46e24b694a11e0d3b351e5509722d167",
+    "url": "/static/media/check.46e24b69.svg"
+  }
+]);
